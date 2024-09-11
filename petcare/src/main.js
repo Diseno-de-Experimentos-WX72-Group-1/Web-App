8fee5e0f9a2288650583e533ee784773232b0a86
Camila Amaro <camilaamarov@yahoo.es>
2024-09-10 19:03:57 -0500

    chore: Added configs

--- petcare/src/main.js
+++ petcare/src/main.js
@@ -1,6 +1,23 @@
 import './assets/main.css'
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css'
+import VueMq from 'vue3-mq'
+
+import PrimeVue from "primevue/config";
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.use(VueMq, {
+    breakpoints: {
+        sm: 640,
+        md: 768,
+        lg: Infinity
+    },
+    defaultBreakpoint: 'sm',
+});
+app.mount('#app');
