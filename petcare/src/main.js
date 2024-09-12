b750ec39fb08d5be77a71b01e35f7b6b79fa79b8
Camila Amaro <camilaamarov@yahoo.es>
2024-09-11 19:44:09 -0500

    fix: Delete duplicated import

--- petcare/src/main.js
+++ petcare/src/main.js
@@ -3,19 +3,17 @@ import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css'
 import VueMq from 'vue3-mq'
 
+import { createApp } from 'vue'
+import App from './App.vue'
 import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
+import InputMask from 'primevue/inputmask'
+import Select from "primevue/select";
+import Dropdown from "primevue/dropdown";
 
-    import { createApp } from 'vue'
-    import App from './App.vue'
-    import PrimeVue from "primevue/config";
-    import Button from "primevue/button";
-    import Toolbar from "primevue/toolbar";
-    import InputText from "primevue/inputtext";
-    import InputMask from 'primevue/inputmask'
-    import Select from "primevue/select";
-    import Dropdown from "primevue/dropdown";
-
-    import router from "@/router/index.js";
+import router from "@/router/index.js";
 
 const app = createApp(App);
 
@@ -28,13 +26,13 @@ app.use(VueMq, {
     defaultBreakpoint: 'sm',
 });
 
-    app.use(PrimeVue); // inyección de depencias
-    app.use(router)
+app.use(PrimeVue); // inyección de depencias
+app.use(router)
 
-    app.component('pv-button',Button)
-    app.component('pv-toolbar', Toolbar)
-    app.component('pv-input-text', InputText)
-    app.component('pv-input-mask', InputMask)
-    app.component('pv-select', Select)
+app.component('pv-button',Button)
+app.component('pv-toolbar', Toolbar)
+app.component('pv-input-text', InputText)
+app.component('pv-input-mask', InputMask)
+app.component('pv-select', Select)
 
-    app.mount('#app')
+app.mount('#app')
