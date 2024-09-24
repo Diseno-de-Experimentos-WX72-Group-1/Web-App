bb9a21f2d444a35d0b0ebdf3f90d8a63b64ead2c
Camila Amaro <camilaamarov@yahoo.es>
2024-09-24 14:59:34 -0500

    fix: Solved error with an import

--- petcare/src/main.js
+++ petcare/src/main.js
@@ -2,44 +2,34 @@ import './assets/main.css'
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css'
 import Aura from '@primevue/themes/aura';
-import VueMq from 'vue3-mq'
 
-    import { createApp } from 'vue'
-    import App from './App.vue'
-    import PrimeVue from "primevue/config";
-    import Button from "primevue/button";
-    import Toolbar from "primevue/toolbar";
-    import InputText from "primevue/inputtext";
-    import InputMask from 'primevue/inputmask'
-    import Select from "primevue/select";
-    import Card from 'primevue/card';
-    import Dropdown from "primevue/dropdown";
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
+import InputMask from 'primevue/inputmask'
+import Select from "primevue/select";
+import Card from 'primevue/card';
+// import Dropdown from "primevue/dropdown";
 
-    import router from "@/router/index.js";
+// import router from "@/router/index.js";
 
 const app = createApp(App);
-
-app.use(VueMq, {
-    breakpoints: {
-        sm: 640,
-        md: 768,
-        lg: Infinity
-    },
-    defaultBreakpoint: 'sm',
-});
 
 app.use(PrimeVue, {
     theme: {
         preset: Aura
     }
 });
-    app.use(router)
+    // app.use(router)
 
-    app.component('pv-button',Button)
-    app.component('pv-toolbar', Toolbar)
-    app.component('pv-input-text', InputText)
-    app.component('pv-input-mask', InputMask)
-    app.component('pv-select', Select)
-    app.component('pv-card', Card)
+app.component('pv-button',Button)
+app.component('pv-toolbar', Toolbar)
+app.component('pv-input-text', InputText)
+app.component('pv-input-mask', InputMask)
+app.component('pv-select', Select)
+app.component('pv-card', Card)
 
-    app.mount('#app')
+app.mount('#app')
